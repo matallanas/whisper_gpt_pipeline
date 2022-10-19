f769c26ef8c629835eb06db6eaaf01daf72faaec
juancopi81 <juancopi_81@hotmail.com>
2022-10-19 14:21:50 -0500

    Add apply method to whisper transcriber class

--- youtube_transcriber/transcribing/whispertranscriber.py
+++ youtube_transcriber/transcribing/whispertranscriber.py
@@ -1,3 +1,9 @@
+import os
+from pathlib import Path
+
+from pytube import YouTube
+import whisper
+
 from youtube_transcriber.transcribing.transcribe import Transcribe
 from youtube_transcriber.video import RawVideo, TranscribedVideo
 
@@ -13,10 +19,40 @@ class WhisperTranscriber(Transcribe):
     """
 
     def __init__(self, model: str="base", without_timestamps: bool=False) -> None:
-        self.model = model
+        self.model = whisper.load_model(model)
         self.without_timestamps = without_timestamps
 
     def apply(self, raw_video: RawVideo) -> TranscribedVideo:
         """Creates a new video with transcriptions created by Whisper.
         """
-        # TODO
+        # Get audio from video
+        try:
+            audio_file = self._get_audio_from_video(raw_video)
+        
+        except Exception as e:
+            print(f"Exception: {e}")
+        
+        result = self.model.transcribe(audio_file, 
+                                       without_timestamps=self.without_timestamps)
+        transcription = result["text"]
+        
+        data = []
+        for seg in result['segments']:
+            data.append({'start': seg['start'], 'end': seg['end'],'text': seg['text']})
+
+        os.remove(audio_file)
+
+        return TranscribedVideo(channel_name = RawVideo.channel_name,
+                                url = RawVideo.url,
+                                transcription = transcription,
+                                segments = data)
+        
+    def _get_audio_from_video(self, raw_video: RawVideo) -> Path:
+        # TODO: Add credits
+        yt = YouTube(raw_video.url)
+        video = yt.streams.filter(only_audio=True).first()
+        out_file = video.download(output_path=".")
+        base, _ = os.path.splitext(out_file)
+        new_file = base + ".mp3"
+        os.rename(out_file, new_file)
+        return new_file
