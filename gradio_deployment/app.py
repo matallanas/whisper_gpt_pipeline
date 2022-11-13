c893389985b59b99ff4fa69120d2e06cef12ba14
Arjun Patel <arjunkirtipatel@gmail.com>
2022-11-13 13:39:20 -0600

    initial commit of demo code

--- gradio_deployment/app.py
+++ gradio_deployment/app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import tensorflow as tf
 import datasets
-
+from notebooks.starter_model_training import tokenize_function, create_dataset, compile_model, gen_text
 '''
 We want to create an application that allows the user to select a dataset from a specified list
 and pass that with specified parameters to a model to train it on.
@@ -11,16 +11,20 @@ Generate some text!
 
 '''
 
+MODEL = ""
 
+def train_model(dat, epochs, model_checkpoint):
+    # trains model given parameters
 
+    model = compile_model(model_checkpoint=model_checkpoint)
 
+    ds = create_dataset(dat)
+    tok_data = tokenize_function(ds, model_checkpoint)
 
-def generate_text():
-    pass
+    mod_history = model.fit(tok_data, epochs = epochs)
+    MODEl = model
 
-
-def train_model():
-    pass
+    return model
 
 
 
@@ -36,19 +40,25 @@ with demo:
     #dataset selector
     with gr.Row():
         with gr.Column():
-            ds = gr.Dropdown(["Whispering-GPT/whisper-transcripts-the-verge"])
+            ds = gr.Dropdown(["Whispering-GPT/whisper-transcripts-the-verge"], value = "Whispering-GPT/whisper-transcripts-the-verge")
             #model parameters
             
             epochs = gr.Slider(minimum = 1, maximum = 10, value  = 1, step = 1, interactive = True)
-            block_size = gr.Dropdown([32, 64, 128, 256])
-            model_checkpoint = gr.Dropdown(["distilgpt2"])
+           #block_size = gr.Dropdown([32, 64, 128, 256], value = 64)
+            model_checkpoint = gr.Dropdown(["distilgpt2"], value = "distilgpt2")
         with gr.Column():
              #train button
             begin_training = gr.Button("Begin Training!")
             #button to generate text iff training done
+            seed_text = gr.Textbox("Write some seed text here")
             create_text = gr.Button("Generate some text!")
     with gr.Row():
         text_output = gr.Textbox(value = "Generated text will appear here")
+        create_text.click(gen_text, inputs = [model_checkpoint, MODEL, seed_text, 3], outputs=[text_output])
+
+    
+    begin_training.click(train_model, inputs = [ds, model_checkpoint, epochs])
+
 
 demo.launch()
 
