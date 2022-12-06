c71c3f98875f6492a828262b52783a7962ad273b
Eduardo Matallanas <eduardo.matallanas@es.nestle.com>
2022-12-06 13:48:46 +0100

    Rename and modify main script

--- src/whisper_gpt.py
+++ src/whisper_gpt.py
@@ -127,55 +127,5 @@ def dataset(
   if upload or (overwrite and name != ""):
     ds.upload()
 
-@cli.command()
-@click.option("--name", "-n", default="",
-    help="Name of the Hugging Face Dataset, if empyt a local dataset is created.")
-@click.option("--token", "-t", default=None,
-    help="token of the repository to read and/or write.")
-@click.option("--input", "-i",
-    help="Location of the data, could be youtube url or a local path.")
-@click.option("--download-path", "-d", default="tmp/",
-    help="Path to download and store files.")
-@click.option("--audio", "-a", is_flag=True, show_default=True, default=False,
-    help="Add audio feature to the dataset with the audio extracted.")
-@click.option("--audio-format", "-f", default="mp3",
-    type=click.Choice(AUDIO_FORMAT, case_sensitive=False),
-    help="Specific audio format")
-@click.option("--upload", "-u", is_flag=True, show_default=True, default=False,
-    help="Upload dataset to the hub.")
-@click.option("--number-videos", "-v", default=0, help="Number of videos to process.")
-@click.option("--overwrite", "-o", is_flag=True, default=False, help="Overwrite the data.")
-@global_options
-def audio_dataset(
-  name: str,
-  token: str,
-  input: str,
-  download_path: str,
-  upload: str,
-  model_size: str,
-  language: str,
-  mode: str,
-  write: bool,
-  audio: bool,
-  audio_format: str,
-  number_videos: int,
-  overwrite: bool
-):
-  """
-  Add the transcript of Youtube videos to a Hugging Face dataset
-  """
-
-  ds = TranscriptDataset(name, token, audio)
-  i = 339
-  print(ds.dataset["train"].select([['id', 'channel', 'channel_id', 'title', 'categories', 'tags', 'description', 'text', 'segments']]))
-  # for e in ds.dataset["train"]:
-  #   title = e["title"]
-  #   if '#' in title:
-  #     print(f'{i} - {title.split("#")[1]}')
-  #   else:
-  #     print(f'{i} - {title}')
-  #   i -= 1 
-
-
 if __name__ == "__main__":
   cli()
